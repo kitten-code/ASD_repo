e3eba8713dd4cb2216796213c6aaf9b3be739b4f
Aleksandra <aleksandra16120@gmail.com>
2021-12-06 23:51:03 +0100

    ResztaAZ
    Wydawanie reszty met. ALgorytmów Zachlannych

---
+++ ResztaAZ/src/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+    final static int zl=13;
+    final static int gr=33;
+
+    public static void main(String[] args) {
+        final int[] M = {500, 200, 100, 50, 20, 10, 5, 2, 1}; // nominaly wyrazone w groszach
+        int  r, i = 0;
+        r = zl * 100 + gr; // konwersja zdefiniowanej reszty na grosze
+
+
+
+        while (r > 0) // dopoki reszta > 0 ...
+        {
+            if (r >= M[i]) // jesli reszta wieksza/rowna danemu nominalowi ...
+            {
+                System.out.print(M[i] / 100.0 + " "); // wypisz ten nominal
+                r = r - M[i]; // i pomniejsz reszte o wydane monety
+            }
+            else
+            {
+                i++; // w przeciwnym przypadku przejdz do kolejnego nominalu
+            }
+        }
+        System.out.println();
+    }
+}
